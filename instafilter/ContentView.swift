c5d146edd956d1f3eec1885110ebabd2a253d679
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2020-06-10 09:37:11 -0700

    Initial Commit

---
+++ instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  instafilter
+//
+//  Created by Aldiyar Batyrbekov on 6/10/20.
+//  Copyright © 2020 Aldiyar Batyrbekov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
